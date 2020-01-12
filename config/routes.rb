334a0373236f7e0ada50868afe59e6131459098e
Fred <fred@travelstart.com>
2020-01-12 09:44:17 +0200

    added a few pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'welcome/home', to: 'welcome#home'
+  get 'welcome/about', to: 'welcome#about'
+
+  get 'login/index', to: 'login#index'
+
+  get 'pages/index', to: 'pages#index'
+
+  # root 'login#index'
 end
